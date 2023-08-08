f289660788310831752b0dd87c3f2a8096734b77
Ssstor <savva.fax@gmail.com>
2023-08-08 13:23:10 +0300

    for WP on scrapy

--- santech/start.py
+++ santech/start.py
@@ -1,11 +1,17 @@
-import argparse
-import os
-
-aparser = argparse.ArgumentParser(description='Запуск парсера')
-aparser.add_argument('url')
-
-url = aparser.parse_args().url 
-
-open(os.path.realpath(__file__).split('start.py')[0] + 'url.txt', 'w').write(url)
-
-os.system('cd ' + os.path.realpath(__file__).split('start.py')[0] + ' && scrapy crawl santech_parser')
+try:
+    import argparse
+    import os
+    
+    aparser = argparse.ArgumentParser(description='Запуск парсера')
+    aparser.add_argument('url')
+    
+    urlll = aparser.parse_args()
+    
+    open(os.path.realpath(__file__).split('start.py')[0] + 'url.txt', 'w', encoding='utf-8').write(urlll.url)
+    
+    os.system('cd ' + os.path.realpath(__file__).split('start.py')[0] + ' && scrapy crawl santech_parser')
+    print('<p align="center"><font color=green>Parsed!<font></p>')
+    
+except Exception as error:
+    print('<p align="center"><font color=red>Error!<font></p>')
+    print(f'<pre>{error}</pre>')
