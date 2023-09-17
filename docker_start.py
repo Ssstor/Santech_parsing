1d11231b9ccd71974fdcbf58344c1a10f3e518e1
Ssstor <savva.fax@gmail.com>
2023-09-17 14:01:45 +0300

    dockerized_OF

--- docker_start.py
+++ docker_start.py
@@ -1,3 +1,13 @@
-import os 
+import subprocess
 
-os.system('docker run -t docker_parser')
+command = 'docker run -t docker_parser'
+
+try:
+    result = subprocess.check_output(command, shell=True).decode('utf-8')
+    print(result)
+except:
+    command = 'docker build -t docker_parser docker_parser'
+    result = subprocess.check_output(command, shell=True).decode('utf-8')
+    subprocess.check_output('docker run -t docker_parser', shell=True).decode('utf-8')
+    print(result)
+ 
